33210f2c19e974c3a25021044f659bfeb5d5c3fb
Naufal Fawwaz <fawaznaufal23@gmail.com>
2023-05-20 17:54:20 +0700

    initial commit

---
+++ Galasin/GalasinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalasinApp.swift
+//  Galasin
+//
+//  Created by Naufal Fawwaz Andriawan on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GalasinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
